d73693644d1dfe834edc7c1e96dc57e0f2224d2b
Fabricio Catae <fcatae@hotmail.com>
2020-10-31 16:32:51 -0300

    add GET with headers

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
-function updateResponse(uri, textResponse) {
+function updateResponse(uri, func, textResponse) {
     textResponse.innerText = "(calling the function)";
 
-    getRemoteData(uri)
+    func(uri)
     .then( res => {
         var title = res.glossary?.title;
         textResponse.innerText = JSON.stringify(res);
@@ -20,3 +20,13 @@ async function getRemoteData(url) {
     return await response.json();
 }
 
+async function getRemoteDataWithHeaders(url) {
+    var response = await fetch(url, {
+            headers: [ ['Authorization', 'Bearer ABC'] ]
+        });
+
+    if( response.status != 200 )
+        throw "HTTP status code != 200"
+
+    return await response.json();
+}
